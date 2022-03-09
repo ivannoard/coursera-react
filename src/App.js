b7c93dc968fcb4ab665d0bbafb44815a0f0463a6
ivan <ivannovaa.24@gmail.com>
2022-03-09 23:50:29 +0700

    making f comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-// import NavBar from './components/NavBar';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import MenuComponent from "./components/MenuComponent";
-import { DISHES } from "./shared/dishes";
+import Main from './components/MainComponent';
 
 function App() {
   return (
     <div className="app">
-      {/* <NavBar /> */}
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <MenuComponent dishes={DISHES} />
+      <Main />
     </div>
   );
 }
